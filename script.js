9b6a45b441230183ba4444f3284a92c0b1d2b19c
CharacterPopularity <cthompson6074@gmail.com>
2026-05-08 14:21:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Import the functions you need from the SDKs you need
+npm install firebase
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFD3pEleXfgxT5BXrcDLU7Pq2EAtX2KA4",
+  authDomain: "character-popularity.firebaseapp.com",
+  projectId: "character-popularity",
+  storageBucket: "character-popularity.firebasestorage.app",
+  messagingSenderId: "34609153629",
+  appId: "1:34609153629:web:8c3a5cf88101b9bd59efb4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const characters = [
   {
     name: "Megumi Fushiguro",
